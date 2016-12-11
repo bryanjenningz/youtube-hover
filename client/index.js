9f48a5263c868a27d3d9b892b9859ee050f71a08
Bryan Jennings <bryanjenningz@gmail.com>
2016-12-10 23:03:51 -0800

    add server and react router

--- client/index.js
+++ client/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import {render} from 'react-dom'
 import App from './components/App'
+import {Router, Route, browserHistory} from 'react-router'
+
+const routes =
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+  </Router>
 
 const rootEl = document.querySelector('#root')
-render(<App />, rootEl)
+render(routes, rootEl)
